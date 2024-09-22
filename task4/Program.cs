a80dc571fb3a391f15b6547f09c800f9fa1d6def
Manvel <Manvelnavoan@gmail.com>
2024-09-22 14:38:27 +0300

    In Homework1 add task4

---
+++ task4/Program.cs
@@ -0,0 +1,39 @@
+﻿// Вывести нечетные числа
+// Описание: Напишите метод, который на вход принимает число (number), а на выходе
+// выводит все нечетные числа от 1 до number (включительно), после каждого числа
+// должен быть знак пробела.
+// Пример использования: На входе:
+// ● number: 5
+// На выходе:
+// ● 1 3 5
+// На входе:
+// ● number: 8
+// На выходе:
+// ● 1 3 5 7
+
+using System;
+public class Answer
+{
+    static void PrintOddNumbers(int number)
+    {
+        // Введите свое решение ниже
+        int i = 1;
+        for ( i = 1 ; i<= number; i+=2)
+        {
+            Console.WriteLine($"{i} ");
+        }
+    }
+    static public void Main(string[] args)
+    {
+        int number;
+        if (args.Length >= 1)
+        {
+            number = int.Parse(args[0]);
+        }
+        else
+        {
+            number = 18;
+        }
+        PrintOddNumbers(number);
+    }
+}
